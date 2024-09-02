3bfc89caebe2534ef88a0ef89059b4f1a99a6272
Abhishek Verma <131652685+Abhishek7242@users.noreply.github.com>
2024-09-02 17:52:24 +0530

    Jelly Slide Button

---
+++ script.js
@@ -0,0 +1,22 @@
+let element = document.querySelector('span');
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2'); // I assume you meant btn2, not btn12
+
+btn2.addEventListener('click', function () {
+    gsap.to(element, {
+        duration: 1.5,
+        ease: "elastic.inOut(1, 0.3)",
+        x: "100%",
+        background: "linear-gradient(239deg, #4caadd ,#5eff35)"
+    });
+});
+btn1.addEventListener('click', function () {
+    gsap.to(element, {
+        duration: 1.5,
+        ease: "elastic.inOut(1, 0.3)",
+        x: "0%",
+        background:" linear-gradient(239deg, #dd4c4c, #d425ff)"
+
+    });
+});
+
